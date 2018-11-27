0673cbca3ba432c83261269e6dafdd378b415df1
James McMeeken <james@jamesmcmeeken.com>
2018-11-27 21:47:30 +0000

    Fix JavaScript linting

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
   Customised FlexSilder controls
 
@@ -8,41 +8,41 @@ var flexSliderClass = document.getElementsByClassName('flexslider');
 
 // Run this only if FlexSlider markup is present
 if (flexSliderClass.length > 0) {
-  $(window).on('load', function () {
+    $(window).on('load', function () {
 
-    // Hold onto this in case client wants this functionality reinstated
-    /*
-    var index = 0, hash = window.location.hash;
+        // Hold onto this in case client wants this functionality reinstated
+        /*
+        var index = 0, hash = window.location.hash;
 
-    if (hash) {
-      index = /\d+/.exec(hash)[0];
-      index = (parseInt(index) || 1) - 1;
-    }
+        if (hash) {
+        index = /\d+/.exec(hash)[0];
+        index = (parseInt(index) || 1) - 1;
+        }
 
-    $('#slider').flexslider(index);
-    */
+        $('#slider').flexslider(index);
+        */
 
-    $('#carousel').flexslider({
-      animation: "slide",
-      controlNav: false,
-      smoothHeight: true,
-      animationLoop: false,
-      slideshow: false,
-      itemWidth: 134,
-      itemMargin: 13,
-      asNavFor: '#slider',
-      controlsContainer: $(".custom-controls-container"),
-      customDirectionNav: $(".custom-navigation a"),
-      useCSS: false
+        $('#carousel').flexslider({
+            animation: 'slide',
+            controlNav: false,
+            smoothHeight: true,
+            animationLoop: false,
+            slideshow: false,
+            itemWidth: 134,
+            itemMargin: 13,
+            asNavFor: '#slider',
+            controlsContainer: $('.custom-controls-container'),
+            customDirectionNav: $('.custom-navigation a'),
+            useCSS: false
+        });
+
+        $('#slider').flexslider({
+            animation: 'slide',
+            animationLoop: false,
+            slideshow: false,
+            sync: '#carousel'
+        });
+
     });
-
-    $('#slider').flexslider({
-      animation: "slide",
-      animationLoop: false,
-      slideshow: false,
-      sync: "#carousel"
-    });
-
-  });
 
 }
